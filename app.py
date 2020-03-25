663f28cd8a5e9afb4a81ffcab9c88305a30bb82b
Joan <joanrafols1988@gmail.com>
2020-03-25 18:13:01 +0100

    Getting there

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import dash
 import numpy as np
 import dash_core_components as dcc
+import dash_table
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
@@ -14,17 +15,70 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 # Load the data in a dataframe structure
-df = pd.read_excel('solventSelectionTool_table.xlsx', sheet = 0, header = 2)
+df = pd.read_excel('solventSelectionTool_table.xlsx', sheet_name = 0, header = 2)
+df2 = pd.read_excel('solventSelectionTool_table.xlsx', sheet_name = 1, header = 0, usecols=(0,1))
 # Drop first row as it is empty
 df = df[1:]
 # Add extra columns with calculated parameters
-df['Hansen coordinates']= [[df['dD - Dispersion'].iloc[i], df['dP - Polarity'].iloc[i], df['dH - Hydrogen bonding'].iloc[i]] for i in range(df.shape[0])]
+df['Hansen coordinates']= [np.array([df['dD - Dispersion'].iloc[i], df['dP - Polarity'].iloc[i], df['dH - Hydrogen bonding'].iloc[i]]) for i in range(df.shape[0])]
 list_GSK_scores = df.columns.values[7:18]
 df['Waste'] = (df['Incineration']*df['Recycling']*df['Biotreatment']*df['VOC Emissions'])**0.25
 df['Environment']  =(df['Aquatic Impact']*df['Air Impact'])**0.5
 df['Health'] = (df['Health Hazard']*df['Exposure Potential'])**0.5
 df['Safety'] = (df['Flammability and Explosion']*df['Reactivity and Stability'])**0.25
 df['Greenness'] = round((df['Waste']*df['Environment']*df['Health']*df['Safety'])**0.25,2)
+df['Ra'] = np.nan
+
+
+def update_Ra(coordinates, reference):
+    """Calculates the Hansen parameter as Ra**2 = 4(dD - dD_0)**2 + (dP - dP_0)**2 + (dH - dH_0)**2.
+        - coordinates: a Dataframe series cointaining a 3-elements array
+        - reference: 3-element vector to which to calculate the distance"""
+    for value in reference:
+        if value == None:
+            return np.nan
+    distance = coordinates - reference
+    Ra = [np.round(np.sqrt(4*d[0]**2 + d[1]**2 + d[2]**2),2) for d in distance]
+    return Ra
+
+def create_report(data = df.loc[25]):
+    # Incides string
+    scores = ''
+    for label in data.index[8:18]:
+        scores += '{:s}: {:.1f}; '.format(label, data.loc[label])
+        
+    # Hazard string
+    hazard_labels = data['Hazard Labels'].split(' ')
+    hazard_html = []
+    for hazard in hazard_labels:
+        text_hazard = df2.Fulltext[df2['Statements'] == hazard].values[0]
+        hazard_html.append('{:s}: {:s}'.format(hazard, text_hazard))
+        hazard_html.append(html.Br())
+        
+    # Precaution string
+    precaution_labels = data['Precautionary Labels'].split(' ')
+    precaution_html = []
+    for precaution in precaution_labels:
+        splitted_precaution = precaution.split('+')
+        text = ''
+        for s_precaution in splitted_precaution:
+            text_precaution = df2.Fulltext[df2['Statements'] == s_precaution].values[0]
+            text += text_precaution
+        precaution_html.append('{:s}: {:s}'.format(precaution, text))
+        precaution_html.append(html.Br())
+
+
+    text = [html.H2('Solvent name: {}'.format(data['Solvent Name'])),
+            html.P('CAS: {}'.format(data['CAS Number'])),
+            html.P('Hansen coordinates: dD = {:.1f}, dP = {:.1f}, dH = {:.1f}'.format(*data['Hansen coordinates'])),
+            html.P('Melting Point: {:.0f}°C \t \t Boiling point:  {:.0f}°C'.format(data['Melting Point (°C)'], data['Boiling Point (°C)'])),
+            html.B('GSK green solvent selection scores'),
+            html.P('Overall score: {:.1f}'.format(data['Greenness'])),
+            html.P(scores),
+            html.B('Globally harmonized System of Classification and Labelling of Chemical'),
+            html.P(hazard_html),
+            html.P(precaution_html)]
+    return text
 
 x = df['dD - Dispersion']
 y = df['dP - Polarity']
@@ -78,18 +132,23 @@ app.layout = html.Div([
                         id = "dD-input",
                         type = 'number',
                         placeholder="dD value",
+                        debounce = True,
+                        style = {'width' : '30%'},
                 ),
                     dcc.Input(
                         id = "dP-input",
                         type = 'number',
                         placeholder="dP value",
+                        debounce = True,
+                        style = {'width' : '30%'},
                     ),
                     dcc.Input(
                         id = "dH-input",
                         type = 'number',
                         placeholder="dH value",
-                    ), 
-                    html.Button('Find known solvent', id='filter-by-solvent-value')
+                        debounce = True,
+                        style = {'width' : '30%'},
+                    )
                 ]),  
                 dcc.Dropdown(
                     id='solvent-list',
@@ -98,47 +157,82 @@ app.layout = html.Div([
                     placeholder = "Choose a solvent...",
                     multi = True
                      ),                
-                html.Button('Find virtual solvent', id='filter-by-solvent-gmean')
+                html.Button('Find virtual solvent', id='virtual-solvent')
                 ],
-            style = {'width' : '40%', 'display': 'inline-block'}
+
+            style = {'width' : '40%', 'display': 'inline-block', 'vertical-align':'top'}
             )            
             ]
-    )
-    
+    ),
+        html.Div( [
+            dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i} for i in df.columns[[-1, -2,0]]],
+            data = df[['Solvent Name', 'Greenness', 'Ra']].to_dict('records'),
+#            fixed_rows = { 'headers': True, 'data': 0},
+            style_as_list_view = True,
+            row_selectable='single',
+            selected_rows = [],
+            style_cell_conditional=[
+            {'if': {'column_id': 'Solvent Name'},
+                'textAlign': 'left','width': '20px','maxWidth': '50px'
+            }],
+            style_table={'overflowY': 'scroll',
+                         'height' : '300px',
+                         'maxHeight': '300px',
+                         'minWidth': '300px',
+                         'maxWidth': '600px',
+                         'border': 'thin lightgrey solid'},
+            style_cell={'minWidth': '0px', 'width': '20px','maxWidth': '50px',
+                        'whiteSpace': 'normal', 'text-align':'center'
+        }
+            ),
+        ], style =  {'width' : '60%'},),
+        html.Div(id = 'report', children = create_report(), 
+             style = {'width':'40%','display': 'inline-block', 'vertical-align':'top'}
+             )
 ])
 
 
 
 @app.callback([Output('main-plot', 'figure'),
-               Output('dD-input', 'value'),
+               Output('table', 'data')],
+              [Input('virtual-solvent', 'n_clicks')],
+              [State('main-plot', 'figure'),
+               State('dD-input', 'value'),
+               State('dP-input', 'value'),
+               State('dH-input', 'value')])
+def display_virtual_solvent(_, figure, dD, dP, dH):
+#    print(dD, dP, dH)
+    df['Ra'] = update_Ra(df['Hansen coordinates'], [dD,dP,dH])
+    figure['data'][1]['x'] = [dD] if dD != None else [np.nan]
+    figure['data'][1]['y'] = [dP] if dP != None else [np.nan]
+    figure['data'][1]['z'] = [dH] if dH != None else [np.nan]
+    data = (df[['Solvent Name', 'Greenness', 'Ra']]).sort_values('Ra').to_dict('records')
+    return figure, data
+
+@app.callback([Output('dD-input', 'value'),
                Output('dP-input', 'value'),
                Output('dH-input', 'value')],
-              [Input('filter-by-solvent-gmean', 'n_clicks')],
-              [State('main-plot', 'figure'),
-               State('solvent-list', 'value')])
-def display_virtual_solvent(_, figure, solvent_index):
+            [Input('solvent-list', 'value')],
+            [State('dD-input', 'value'),
+             State('dP-input', 'value'),
+             State('dH-input', 'value')])
+def update_hansen_parameters_by_list(solvent_index,  dD, dP, dH):
     N = len(solvent_index)
-    if N == 0:
-        figure['data'][1]['x'] = [np.nan]
-        figure['data'][1]['y'] = [np.nan]
-        figure['data'][1]['z'] = [np.nan]
-        dD, dP, dH = None, None, None
-    else:
-        
-        virtual_solvent = np.array(df['Hansen coordinates'].loc[solvent_index[0]])
-        for p in solvent_index[1:]:
-            virtual_solvent += np.array(p)
-        virtual_solvent = virtual_solvent/N
-        figure['data'][1]['x'] = [virtual_solvent[0]]
-        figure['data'][1]['y'] = [virtual_solvent[1]]
-        figure['data'][1]['z'] = [virtual_solvent[2]]
-        dD, dP, dH = [round(i,2) for i in virtual_solvent]
-    return figure, dD, dP, dH
+    if N > 0:
+        dD, dP, dH = df['Hansen coordinates'].loc[solvent_index].mean().round(2)
+#        df['Ra'] = update_Ra(df['Hansen coordinates'], [dD,dP,dH])
+    return  dD, dP, dH
 
-#@app.callback([Output('main-plot', 'figure')],
-#              [Input('update-plot', 'n_clicks')],
-#              [State('main-plot', 'figure')])
-#def update_plot_, figure, )
+@app.callback(Output('report', 'children'),
+             [Input('table','selected_rows')])
+def update_report(selected_row):
+    if selected_row == []:
+        return ''
+    else:
+        return create_report(df.iloc[selected_row[0]])
+
 
 
 if __name__ == '__main__':
